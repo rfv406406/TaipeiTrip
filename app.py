097704125c6af86698be74513b36376e81bbd22d
rfv406406 <rfv406406@gmail.com>
2023-09-27 13:16:05 +0800

    API DONE

--- app.py
+++ app.py
@@ -281,39 +281,109 @@ def user_auth():
             user_email = payload.get('email')
 
             return jsonify({"data":{'id': user_id, 'name': user_name, 'email': user_email}}), 200
+        except ExpiredSignatureError:
+            return ({"error": True, "message": "Token is expired"}),401
         except Exception:
             return jsonify(None), 400
 
-@app.route("/api/booking")
+@app.route("/api/booking", methods=["GET","PUT","DELETE"])
 
 def api_booking():
-    try:
-        data = request.json
-        name = data["signonName"]
-        email = data["signonEmail"]
-        password = data["signonPassword"]
+    if request.method == "POST":
+        try:
+            auth_header = request.headers.get('Authorization')
+            if auth_header is None:
+                return ({"error": True,"message": "please sign in"}), 403
+            else:
+                token = auth_header.split(' ')[1]
+                payload = decode_token(token)
+                member_id = payload.get('id')
 
-        connection = con.get_connection()
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT email FROM member WHERE email= %s", (email, ))
-        data = cursor.fetchone()
+            data = request.json
+            if not data:
+                return ({"error": True,"message": "data is not existed"}), 400
+            
+            attractionId = data["attractionId"]
+            date = data["date"]
+            time = data["time"]
+            price = data["price"]
 
-        if data is None:
-            cursor.execute("INSERT INTO member(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
+            connection = con.get_connection()
+            cursor = connection.cursor(dictionary=True)
+
+            cursor.execute("INSERT INTO booking(member_id, attractionId, date, time, price) VALUES(%s, %s, %s, %s, %s)", (member_id, attractionId, date, time, price))
             connection.commit()
             cursor.close()
             connection.close()
+
+            return jsonify({"ok": True}), 200
+        
+        except mysql.connector.Error:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+            return jsonify({"error": True,"message": "databaseError"}), 500
+
+    if request.method == "GET":
+        try:
+            auth_header = request.headers.get('Authorization')
+            if auth_header is None:
+                return ({"error": True,"message": "please sign in"}), 403
+            else:
+                token = auth_header.split(' ')[1]
+                payload = decode_token(token)
+                member_id = payload.get('id')
+
+            connection = con.get_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT booking.attractionId, booking.date, booking.time, booking.price, attractions.name, attractions.address, images.URL_image "
+            "FROM booking "
+            "LEFT JOIN attractions ON attractions.id = booking.attractionId "
+            "LEFT JOIN images ON images.attractions = booking.attractionId "
+            "WHERE booking.member_id = %s",
+            (member_id,))
+            data = cursor.fetchone()
+            print(data)
+            
+            keys = ["attractionId", "name", "address", "URL_image"]
+            keys_data = {k: data[k] for k in keys}
+
+            return jsonify({"data":{"attraction":keys_data, "date":data["date"], "time":data["time"], "price":data["price"]}}), 200
+            # return jsonify({"data":{"attraction":{"id": data["attractionId"], "name": data["name"], "address":data["address"], "image":data["URL_image"]},
+            #                             "date":data["date"], "time":data["time"], "price":data["price"]}}), 200
+        except mysql.connector.Error:
+            return jsonify({"error": True,"message": "databaseError"}), 500
+        
+        finally:
+            cursor.close()
+            connection.close()
+    
+    if request.method == "DELETE":
+        try:
+            auth_header = request.headers.get('Authorization')
+            if auth_header is None:
+                return ({"error": True,"message": "please sign in"}), 403
+            else:
+                token = auth_header.split(' ')[1]
+                payload = decode_token(token)
+                member_id = payload.get('id')
+
+            connection = con.get_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("DELETE FROM booking WHERE id = %s", (member_id, ))
+            connection.commit()
+
+            if cursor.rowcount == 0: #如無對應id，該語句將成功執行，但不會刪除任何記錄
+                return jsonify({"error": True, "message": "No record found to delete"}), 404
+
             return jsonify({"ok":True}), 200
-        else:
+        except mysql.connector.Error:
+            return jsonify({"error": True,"message": "databaseError"}), 500
+        
+        finally:
             cursor.close()
             connection.close()
-            return jsonify({"error": True,"message": "Email已經註冊帳戶"}), 400
-    except mysql.connector.Error:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-        return jsonify({"error": True,"message": "databaseError"}), 500
         
 app.run(debug=None, host="0.0.0.0", port=3000)
 # app.run(debug = True, port = 3000)
