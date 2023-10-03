14eb18ea5813e03a686d15c8f0e15d3da84c3eb4
Zhui Kang <rfv406406@gmail.com>
2023-10-03 17:55:18 +0800

    6-3

--- static/JS/Tappay.js
+++ static/JS/Tappay.js
@@ -1,8 +1,11 @@
-TPDirect.setupSDK(APP_ID, 'APP_KEY', 'sandbox')
+const APP_ID = 137033;
+const APP_KEY = app_g5H5hXkKSIHANVJsYh99hPcebudiWGo3YokDL3zG8kxYMZT4bX4bQoKJbi7V;
+
+TPDirect.setupSDK(APP_ID, APP_KEY, 'sandbox')
 
 TPDirect.card.setup({
     // Display ccv field
-    fields = {
+    fields: {
         number: {
             // css selector
             element: '#card-number',
@@ -24,5 +27,59 @@ TPDirect.card.setup({
     maskCreditCardNumberRange: {
         beginIndex: 6,
         endIndex: 11
+    }
+});
+
+let payButton = document.querySelector('.pay_button')
+
+// payButton.addEventListener('click', onClick);
+
+// function onClick() {
+//     // 讓 payButton 之後觸發 getPrime 方法
+//     TPDirect.card.getPrime(function (result) {
+//         if (result.status !== 0) {
+//             console.err('getPrime 錯誤')
+//             return
+//         }
+//         var prime = result.card.prime
+//         alert('getPrime 成功: ' + prime)
+//     })
+// }
+
+TPDirect.card.onUpdate(function (update) {
+    if (update.canGetPrime) {
+        payButton.removeAttribute('disabled');
+    } else {
+        payButton.setAttribute('disabled', true);
+    }
+
+    cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unionpay','unknown']
+    if (update.cardType === 'visa') {
+    // Handle card type visa.
+    }
+
+    // number 欄位是錯誤的
+    if (update.status.number === 2) {
+        setNumberFormGroupToError()
+    } else if (update.status.number === 0) {
+        setNumberFormGroupToSuccess()
+    } else {
+        setNumberFormGroupToNormal()
+    }
+
+    if (update.status.expiry === 2) {
+        setNumberFormGroupToError()
+    } else if (update.status.expiry === 0) {
+        setNumberFormGroupToSuccess()
+    } else {
+        setNumberFormGroupToNormal()
+    }
+
+    if (update.status.ccv === 2) {
+        setNumberFormGroupToError()
+    } else if (update.status.ccv === 0) {
+        setNumberFormGroupToSuccess()
+    } else {
+        setNumberFormGroupToNormal()
     }
 });
